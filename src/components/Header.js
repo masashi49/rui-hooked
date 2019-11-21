0a56e6204c4fa45b9ef67ed3528dba25483ee42e
yamada <yamada@reas.jp>
2019-11-21 09:57:19 +0900

    最初のコミット header作成まで https://www.freecodecamp.org/news/how-to-build-a-movie-search-app-using-react-hooks-24eb72ddfaf7/

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (prop) =>{
+  return (
+      <header className='App-header'>
+        <h2>{prop.text}</h2>
+      </header>
+  )
+}
+
+export default Header;
